04ffb21e8257d8cb203c2f0758515c824a1df6c3
Kafui-Ayegbe-dev <kafuiayegbedev@gmail.com>
2025-05-27 09:44:25 -0600

    fixed bug that prevented context from rendering

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new CaretPosition(100,100,30,50);
+const car = new Car(100,100,30,50);
 car.draw(ctx);
